61e701bda926f756bbb515148cf21b33d4623f4b
SharmaAishna <aishna_sharma@live.com>
2022-12-12 11:52:15 +0100

    profile page created

--- BlazorClassLibrary/DataAccess/ISuggestionData.cs
+++ BlazorClassLibrary/DataAccess/ISuggestionData.cs
@@ -7,6 +7,7 @@ public interface ISuggestionData
     Task<List<SuggestionModel>> GetAllSuggestion();
     Task<List<SuggestionModel>> GetAllSuggestionWaitingForApproval();
     Task<SuggestionModel> GetSuggestion(string id);
+    Task<List<SuggestionModel>> GetUsersSuggestions(string userId);
     Task UpdateSuggestion(SuggestionModel suggestion);
     Task UpVoteSuggestion(string suggestionId, string userId);
 }
